77647d4093774a0d29d0db2501081af62d1c13e2
Ella <maxutong16@otcaix.iscas.ac.cn>
2018-06-08 09:57:00 +0800

    remove bc from timestamp calculation and add output notice in update function

--- config/99-update.zsh
+++ config/99-update.zsh
@@ -7,11 +7,12 @@ function update_config_repository_timestamp() {
 
 # the update process
 function update_config_repository() {
-    screen -S test -d -m bash -c "cd $WHAT_THE_ZSH/ && git pull"
+    echo "$fg_bold[white]Updating what-the-zsh in background...$reset_color"
+    screen -S test -d -m bash -c "cd $WHAT_THE_ZSH/ && git pull" && screen -ls
 }
 
 # update config every 15 days
-if (( $(date +%s) >= $(echo `cat $WHAT_THE_ZSH/.last-update` + 1296000 | bc) )); then
+if (( $(date +%s) >= $((`cat $WHAT_THE_ZSH/.last-update` + 1296000)) )); then
     update_config_repository_timestamp
     update_config_repository
 fi
