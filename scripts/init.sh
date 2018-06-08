1ca437325667885e65204c480d22fbba3e902c2a
ella <maxutong16@otcaix.iscas.ac.cn>
2018-06-08 09:41:47 +0800

    add last-update and add an empty file in local function directory

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,7 +8,10 @@ export WHAT_THE_ZSH=`pwd`
 if [[ -f ~/.zshrc ]]; then
     mv ~/.zshrc ~/.zshrc.backup
 fi
+
 ln -s $WHAT_THE_ZSH/scripts/zshrc ~/.zshrc
 ln -sf $WHAT_THE_ZSH/themes/puella.zsh-theme ../themes/
 touch $WHAT_THE_ZSH/config/10-alias.zsh/local.zsh
 mkdir -p $WHAT_THE_ZSH/config/20-function.zsh/local.zsh/
+touch $WHAT_THE_ZSH/config/20-function.zsh/local.zsh/local.zsh
+date +%s > $WHAT_THE_ZSH/.last-update
