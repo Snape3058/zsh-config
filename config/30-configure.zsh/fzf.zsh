13048319560045a1b6849029aebaabccd9d975a9
Ella <maxutong16@otcaix.iscas.ac.cn>
2020-07-28 17:54:53 +0800

    add .clangd to fzf exception list

--- config/30-configure.zsh/fzf.zsh
+++ config/30-configure.zsh/fzf.zsh
@@ -15,7 +15,7 @@ if [ -n "$TMUX" ]; then
 fi
 
 # exclude .git .svn ./[Bb]uild/ by default
-export FZF_DEFAULT_COMMAND='find . -type f | egrep -v "\.\<git\>/|\.\<svn\>/|\./[Bb]uild/"'
+export FZF_DEFAULT_COMMAND='find . -type f | egrep -v "\.\<git\>/|\.\<svn\>/|\./[Bb]uild/|\.clangd/"'
 export FZF_DEFAULT_OPTS="--height 22 --reverse --preview 'source-highlight --failsafe -f esc256 --infer-lang --style-file=esc256.style -n -i {} | head -20'"
-export FZF_CTRL_T_COMMAND='find . -type f | egrep -v "\.\<git\>/|\.\<svn\>/|\./[Bb]uild/"'
+export FZF_CTRL_T_COMMAND='find . -type f | egrep -v "\.\<git\>/|\.\<svn\>/|\./[Bb]uild/|\.clangd/"'
 export FZF_CTRL_T_OPTS="--height 22 --reverse --preview 'source-highlight --failsafe -f esc256 --infer-lang --style-file=esc256.style -n -i {} | head -20'"
